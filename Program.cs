e97a810748b31767014bc1b5f8f256889e73d55d
Utku Kaçar <93051057+UtkuKacar@users.noreply.github.com>
2025-05-01 23:51:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,21 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1) EF Core DbContext'i DI konteynerine kaydediyoruz:
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     )
 );
 
-// 2) Diğer servisler
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 3) Geliştirme ortamı için hata sayfası ve Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
